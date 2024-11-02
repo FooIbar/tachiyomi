7eb8c8f80672a45a3d445e56fa2a8097c7eb768f
FooIbar <118464521+FooIbar@users.noreply.github.com>
2024-11-02 08:19:29 +0000

    Fix build

--- buildSrc/src/main/kotlin/AndroidConfig.kt
+++ buildSrc/src/main/kotlin/AndroidConfig.kt
@@ -3,5 +3,5 @@ object AndroidConfig {
     const val compileSdk = 35
     const val minSdk = 23
     const val targetSdk = 35
-    const val ndk = "27.1.12297006"
+    const val ndk = "27.2.12479018"
 }
